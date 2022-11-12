574a3de422f9c932abbcf714145bebbc3145effe
Helen Sherstuik <76433212+Spriggan01@users.noreply.github.com>
2022-11-13 01:12:22 +0200

    Delete Шерстюк_lab7 directory

--- Шерстюк_lab7/data/declare-data.php
+++
@@ -1,4 +0,0 @@
-<?php
-$data['actor'] = require(__DIR__ .'/declare-actor.php');
-require(__DIR__ .'/declare-films.php');
-?>
--- Шерстюк_lab7/forms/delete-film.php
+++
@@ -1,4 +0,0 @@
-<?php
-unlink(__DIR__ . "/../data/" . $_GET['actor'] . "/" . $_GET['file']);
-header('Location: ../index.php?actor=' . $_GET['actor']);
-?>
